73b2832ea80365828bdf9f59feb73556396934ed
chavda samarth <chavdasamarth@chavdas-MacBook-Air.local>
2026-03-07 15:18:46 +0530

    Revert: remove AI Module from repository

--- AI Module/backend/product.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createProduct } = require('./product.controller');
-
-const router = express.Router();
-
-router.post('/', createProduct);
-
-module.exports = router;
